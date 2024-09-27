b57b99878337c1aa125aad944f8eab2eecb49b86
Bradford Derby <derby.b@northeastern.edu>
2024-09-27 11:20:39 -0400

    amazing work!

--- src/calculator.tsx
+++ src/calculator.tsx
@@ -1,13 +1,24 @@
-import './calculator.css';
-import { CalculatorDisplay, CalculatorKey } from './components';
-import { NumericKeys } from './enums/numeric-keys.enum';
-import { OperatorKeys } from './enums/operator-keys.enum';
-import { ActionKeys } from './enums/action-keys.enum';
-import { useState } from 'react';
-
+import "./calculator.css";
+import { CalculatorDisplay, CalculatorKey } from "./components";
+import { NumericKeys } from "./enums/numeric-keys.enum";
+import { OperatorKeys } from "./enums/operator-keys.enum";
+import { ActionKeys } from "./enums/action-keys.enum";
+import { useState } from "react";
 
 export default function Calculator() {
   const [display, setDisplay] = useState("0");
+
+  function numericKeyPressHandler(key: NumericKeys): void {
+    setDisplay(key);
+  }
+
+  function operatorKeyPressHandler(key: OperatorKeys): void {
+    setDisplay(key);
+  }
+
+  function actionKeyPressHandler(key: ActionKeys): void {
+    setDisplay(key);
+  }
 
   function dummy(key: NumericKeys | OperatorKeys | ActionKeys): void {
     alert(`${key}-key pressed.`);
@@ -24,74 +35,74 @@ export default function Calculator() {
               <CalculatorKey
                 className="key-clear"
                 label={ActionKeys.CLEAR}
-                onPress={dummy}
+                onPress={actionKeyPressHandler}
               />
               <CalculatorKey
                 className="key-sign"
                 label={ActionKeys.SIGN_FLIP}
-                onPress={dummy}
+                onPress={actionKeyPressHandler}
               />
               <CalculatorKey
                 className="key-percent"
                 label={OperatorKeys.SQRT}
-                onPress={dummy}
+                onPress={operatorKeyPressHandler}
               />
             </div>
             <div className="digit-keys">
               <CalculatorKey
                 className="key-0"
                 label={NumericKeys.ZERO}
-                onPress={dummy}
+                onPress={numericKeyPressHandler}
               />
               <CalculatorKey
                 className="key-dot"
                 label={ActionKeys.DOT}
-                onPress={dummy}
+                onPress={numericKeyPressHandler}
               />
               <CalculatorKey
                 className="key-1"
                 label={NumericKeys.ONE}
-                onPress={dummy}
+                onPress={numericKeyPressHandler}
               />
               <CalculatorKey
                 className="key-2"
                 label={NumericKeys.TWO}
-                onPress={dummy}
+                onPress={numericKeyPressHandler}
               />
               <CalculatorKey
                 className="key-3"
                 label={NumericKeys.THREE}
-                onPress={dummy}
+                onPress={numericKeyPressHandler}
               />
               <CalculatorKey
                 className="key-4"
                 label={NumericKeys.FOUR}
-                onPress={dummy}
+                onPress={numericKeyPressHandler}
               />
               <CalculatorKey
                 className="key-5"
                 label={NumericKeys.FIVE}
-                onPress={dummy}
+                onPress={numericKeyPressHandler}
               />
               <CalculatorKey
                 className="key-6"
                 label={NumericKeys.SIX}
-                onPress={dummy}
+                onPress={numericKeyPressHandler}
               />
               <CalculatorKey
                 className="key-7"
                 label={NumericKeys.SEVEN}
-                onPress={dummy}
+                onPress={numericKeyPressHandler}
               />
               <CalculatorKey
                 className="key-8"
                 label={NumericKeys.EIGHT}
-                onPress={dummy}
+                onPress={numericKeyPressHandler}
               />
               <CalculatorKey
                 className="key-9"
                 label={NumericKeys.NINE}
-                onPress={dummy}
+                onPress={numericKeyPressHandler}
               />
             </div>
           </div>
@@ -99,27 +110,27 @@ export default function Calculator() {
             <CalculatorKey
               className="key-divide"
               label={OperatorKeys.DIV}
-              onPress={dummy}
+              onPress={operatorKeyPressHandler}
             />
             <CalculatorKey
               className="key-multiply"
               label={OperatorKeys.MULT}
-              onPress={dummy}
+              onPress={operatorKeyPressHandler}
             />
             <CalculatorKey
               className="key-subtract"
               label={OperatorKeys.MINUS}
-              onPress={dummy}
+              onPress={operatorKeyPressHandler}
             />
             <CalculatorKey
               className="key-add"
               label={OperatorKeys.PLUS}
-              onPress={dummy}
+              onPress={operatorKeyPressHandler}
             />
             <CalculatorKey
               className="key-equals"
               label={ActionKeys.EQUALS}
-              onPress={dummy}
+              onPress={actionKeyPressHandler}
             />
           </div>
         </div>
